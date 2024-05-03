616cfb5d5662832859ef574aefab9c8f92ef297b
romak maharjan <romakmaharjan@gmail.com>
2024-05-03 16:24:49 +0545

    code updates

--- resources/views/backend/components/aside.blade.php
+++ resources/views/backend/components/aside.blade.php
@@ -5,14 +5,14 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="index.html">
+        <a class="nav-link " href="{{route("dashboard")}}">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
       @if(auth()->user()->role == "admin")
       <li class="nav-item">
-        <a class="nav-link " href="index.html">
+        <a class="nav-link " href="{{route("users")}}">
           <i class="bi bi-people"></i>
           <span>Users</span>
         </a>
